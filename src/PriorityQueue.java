a5e30a7a8af56416531d1fd06771ee37f1c7d690
SweetBabyDave <david.j.merkley@gmail.com>
2022-03-18 11:44:08 -0600

    Initial commit

---
+++ src/PriorityQueue.java
@@ -0,0 +1,77 @@
+public class PriorityQueue<E extends Comparable<E>> {
+    private Node<E> head;
+
+    public void enqueue(E value) {
+        Node<E> node = new Node<>(value);
+        if (head == null) {
+            head = node;
+        } else {
+            merge(head, node);
+        }
+    }
+
+    public E dequeue() {
+        Node<E> front = head;
+        head = null;
+        head = merge(front.right, front.left);
+        return front.value;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    private Node<E> merge(Node<E> t1, Node<E> t2) {
+        Node<E> small;
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+        if (t1.value.compareTo(t2.value) < 0) {
+            t1.right = merge(t1.right, t2);
+            small = t1;
+        }
+        else {
+            t2.right = merge(t2.right, t1);
+            small = t2;
+        }
+        if (getNpl(small.left) < getNpl(small.right))
+            swapKids(small);
+        setNullPathLength(small);
+        return small;
+    }
+
+    private void setNullPathLength(Node<E> small) {
+
+    }
+
+    private void swapKids(Node<E> small) {
+
+    }
+
+    private int getNpl(Node<E> t) {
+        if (t == null) return -1;
+        return t.npl;
+    }
+
+    private static class Node<E extends Comparable<E>> implements Comparable<Node<E>> {
+        public E value;
+        public Node<E> left;
+        public Node<E> right;
+        public int npl;
+
+        public Node(E value) {
+            this(value, null, null);
+        }
+
+        public Node(E value, Node<E> left, Node<E> right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+            npl = 0;
+        }
+
+        @Override
+        public int compareTo(Node<E> o) {
+            return value.compareTo(o.value);
+        }
+    }
+}
